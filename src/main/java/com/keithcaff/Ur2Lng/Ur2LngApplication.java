6027cb8d0e8d18029fe4a3c06a8b09ca8ddb44af
keithCaff <keithcaffrey@gmail.com>
2020-10-21 21:10:43 +0100

    adding initial project setup from spring initalizer

---
+++ src/main/java/com/keithcaff/Ur2Lng/Ur2LngApplication.java
@@ -0,0 +1,13 @@
+package com.keithcaff.Ur2Lng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ur2LngApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ur2LngApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/keithcaff/Ur2Lng/Ur2LngApplicationTests.java
@@ -0,0 +1,13 @@
+package com.keithcaff.Ur2Lng;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ur2LngApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
